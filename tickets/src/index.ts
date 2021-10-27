b91bfc79ca47b650782aad5821e65812e2e0c802
vasilyevmichael78 <vasilyevmichael78@gmail.com>
2021-10-27 17:52:23 +0300

    update test

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -4,7 +4,7 @@ import { natsWrapper } from './nats-wrapper'
 import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { OrderCanceledListener } from './events/listeners/order-canceled-listener'
 const start = async () => {
-    console.log('starting...')
+    console.log('starting.....')
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined')
     }
